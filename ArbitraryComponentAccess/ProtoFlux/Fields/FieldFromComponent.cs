ca5d2f9a8cfd922c1d181b9f720be5ca3bf7c86e
paradoxical-autumn <110392755+paradoxical-autumn@users.noreply.github.com>
2024-10-31 17:57:29 +0000

    NO FREAKING WAY!!

--- ArbitraryComponentAccess/ProtoFlux/Fields/FieldFromComponent.cs
+++ ArbitraryComponentAccess/ProtoFlux/Fields/FieldFromComponent.cs
@@ -6,24 +6,26 @@ using ProtoFlux.Runtimes.Execution;
 namespace ArbitraryComponentAccess.ProtoFlux.Fields;
 
 [NodeCategory("ACA/Fields")]
-public class FieldFromComponent<T> : ObjectFunctionNode<FrooxEngineContext, T> where T : class, IWorldElement
+public class FieldFromComponent : ObjectFunctionNode<FrooxEngineContext, IField>
 {
     public ObjectInput<Component> Component;
     public ObjectInput<string> Field;
 
-    protected override T Compute(FrooxEngineContext context)
+    protected override IField Compute(FrooxEngineContext context)
     {
         Component my_com = Component.Evaluate(context);
         string my_field = Field.Evaluate(context);
 
         if (my_com == null || my_field == null) return null;
 
-        object pi = ACAReflections.GetProp(my_com, my_field);
+        IField field = my_com.TryGetField(my_field);
 
-        if (pi is T)
-        {
-            return (T)pi;
-        }
-        return null;
+        return field;
+
+        //if (pi is T)
+        //{
+        //    return (T)pi;
+        //}
+        //return null;
     }
 }
