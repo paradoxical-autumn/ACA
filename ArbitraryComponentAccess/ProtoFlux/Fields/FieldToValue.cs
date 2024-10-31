5ad1cbb20d9c53311e4cd7006980ab39e57e4561
paradoxical-autumn <110392755+paradoxical-autumn@users.noreply.github.com>
2024-10-31 18:30:11 +0000

    FieldToValue

---
+++ ArbitraryComponentAccess/ProtoFlux/Fields/FieldToValue.cs
@@ -0,0 +1,23 @@
+﻿using FrooxEngine;
+using FrooxEngine.ProtoFlux;
+using ProtoFlux.Core;
+using ProtoFlux.Runtimes.Execution;
+
+namespace ArbitraryComponentAccess.ProtoFlux.Fields;
+
+[NodeCategory("ACA/Fields")]
+public class FieldToValue<T> : ValueFunctionNode<FrooxEngineContext, T> where T : unmanaged
+{
+    public ObjectInput<IField> Field;
+
+    protected override T Compute(FrooxEngineContext context)
+    {
+        if (Field.Evaluate(context) == null) return default;
+
+        if (Field.Evaluate(context).BoxedValue is T t)
+        {
+            return (T) t;
+        }
+        return default;
+    }
+}
