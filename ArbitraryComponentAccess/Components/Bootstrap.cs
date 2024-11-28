2eabfcee46811134267739a6c983477313511e4d
paradoxical-autumn <110392755+paradoxical-autumn@users.noreply.github.com>
2024-11-28 22:36:43 +0000

    please can someone look at this and tell me what im doing wrong

--- ArbitraryComponentAccess/Components/Bootstrap.cs
+++ ArbitraryComponentAccess/Components/Bootstrap.cs
@@ -1,5 +1,8 @@
 ﻿using FrooxEngine;
-using System.Reflection;
+using FrooxEngine.UIX;
+using System.Threading;
+using Elements.Core;
+using ArbitraryComponentAccess.Components;
 
 namespace ArbitraryComponentAccess;
 
@@ -17,15 +20,68 @@ internal static class ExecutionHook
     }
 #pragma warning restore CS0169, IDE0051, CA1823, IDE0044
 
+    private static void HandleThreadFinished(object sender, EventArgs e)
+    {
+        UniLogPlugin.Log("welcome back to main!!");
+        UserspaceRadiantDash URD = Userspace.UserspaceWorld?.GetGloballyRegisteredComponent<UserspaceRadiantDash>()!;
+        ModalOverlayManager mgr = URD.Slot.GetComponent<ModalOverlayManager>();
+
+        RectTransform rect = mgr.OpenModalOverlay(new float2(0.6f, 0.6f), "ACA Bootstrapper", false, true)!;
+        if (rect != null)
+        {
+            ACAWarningDialog obj = rect.Slot.AttachComponent<ACAWarningDialog>();
+        }
+        else
+        {
+            UniLogPlugin.Error("erm the rect was null??");
+        }
+    }
+
     static ExecutionHook()
     {
         UniLogPlugin.Log("Start of execution hook!!");
-        while (true)
+
+        UserspaceCheckWorker worker = new UserspaceCheckWorker();
+        worker.JobFinished += HandleThreadFinished;
+
+        Thread thread1 = new Thread(worker.Run);
+        thread1.Start();
+}
+
+    class UserspaceCheckWorker
+    {
+        public event EventHandler JobFinished;
+
+        public void Run()
         {
-            if (Userspace.UserspaceWorld != null)
+            Thread.CurrentThread.IsBackground = true;
+            while (Userspace.UserspaceWorld == null)
             {
-                break;
+                UniLogPlugin.Log("userspace world not there!!");
+                Thread.Sleep(1000);
             }
+
+            UniLogPlugin.Log("USERSPACE WORLD THERE!!");
+
+            UniLogPlugin.Log("checking for URD");
+            UserspaceRadiantDash URD = Userspace.UserspaceWorld?.GetGloballyRegisteredComponent<UserspaceRadiantDash>()!;
+
+            while (URD == null)
+            {
+                UniLogPlugin.Log("urd null, checking in 1sec");
+                Thread.Sleep(1000);
+                URD = Userspace.UserspaceWorld?.GetGloballyRegisteredComponent<UserspaceRadiantDash>()!;
+            }
+
+            UniLogPlugin.Log("urd isnt null anymore :D -- checking MOM (modal overlay mgr, not mommy)");
+
+            while (URD.Slot.GetComponent<ModalOverlayManager>() == null)
+            {
+                UniLogPlugin.Log("erm overlay returned null so it no worky :P -- trying again in 1 sec");
+                Thread.Sleep(1000);
+            }
+
+            JobFinished(this, EventArgs.Empty);
         }
     }
 
