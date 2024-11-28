a89bfe392e79f9899bf4d168d6911ec12a40b128
paradoxical-autumn <110392755+paradoxical-autumn@users.noreply.github.com>
2024-11-28 20:32:17 +0000

    create bootstrapper but idk how to wait for userspace to load

---
+++ ArbitraryComponentAccess/Components/Bootstrap.cs
@@ -0,0 +1,43 @@
+﻿using FrooxEngine;
+using System.Reflection;
+
+namespace ArbitraryComponentAccess;
+
+[ImplementableClass(true)]
+internal static class ExecutionHook
+{
+#pragma warning disable CS0169, IDE0051, CA1823, IDE0044
+    // fields must exist due to reflective access
+    private static Type? __connectorType;
+    private static Type? __connectorTypes;
+
+    private static DummyConnector InstantiateConnector()
+    {
+        return new DummyConnector();
+    }
+#pragma warning restore CS0169, IDE0051, CA1823, IDE0044
+
+    static ExecutionHook()
+    {
+        UniLogPlugin.Log("Start of execution hook!!");
+        while (true)
+        {
+            if (Userspace.UserspaceWorld != null)
+            {
+                break;
+            }
+        }
+    }
+
+    private sealed class DummyConnector : IConnector
+    {
+#pragma warning disable CS8766
+        public IImplementable? Owner { get; private set; }
+#pragma warning restore CS8766
+        public void ApplyChanges() { }
+        public void AssignOwner(IImplementable owner) => Owner = owner;
+        public void Destroy(bool destroyingWorld) { }
+        public void Initialize() { }
+        public void RemoveOwner() => Owner = null;
+    }
+}
