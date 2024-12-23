ec998286d1b926a7a5b8fce3bace7b50be1104e6
paradoxical-autumn <110392755+paradoxical-autumn@users.noreply.github.com>
2024-12-23 22:27:10 +0000

    add more default blocked types

--- ArbitraryComponentAccess/Components/ACAPermissions.cs
+++ ArbitraryComponentAccess/Components/ACAPermissions.cs
@@ -1,6 +1,7 @@
 ﻿using FrooxEngine;
 using Elements.Core;
 using System.Security.Policy;
+using FrooxEngine.CommonAvatar;
 
 namespace ArbitraryComponentAccess.Components;
 
@@ -31,8 +32,26 @@ public class ACAPermissions : PermissionsComponent
 
     public void SetupPermissions()
     {
+        // default aca allowed to true
         is_ACA_Allowed.Value = true;
+
+        // prevent ACAing into this component
         BlockedTypes.AddUnique(typeof(ACAPermissions));
+
+        // all other permissions
+        BlockedTypes.AddUnique(typeof(AvatarObjectPermissions));
+        BlockedTypes.AddUnique(typeof(CameraPermissions));
+        BlockedTypes.AddUnique(typeof(GrabbablePermissions));
+        BlockedTypes.AddUnique(typeof(InteractionHandlerPermissions));
+        BlockedTypes.AddUnique(typeof(LocomotionPermissions));
+        BlockedTypes.AddUnique(typeof(ScreenViewPermissions));
+        BlockedTypes.AddUnique(typeof(TouchablePermissions));
+        BlockedTypes.AddUnique(typeof(UserPermissions));
+        BlockedTypes.AddUnique(typeof(VoicePermission));
+        BlockedTypes.AddUnique(typeof(WorldPermissions));
+
+        // other types that should be locked
+        BlockedTypes.AddUnique(typeof(SimpleAvatarProtection));
     }
 
     protected override void OnAttach()
