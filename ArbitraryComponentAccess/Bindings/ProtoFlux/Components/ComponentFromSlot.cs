9ccc426dfc647aa32088103accf28e6dacbc7900
paradoxical-autumn <110392755+paradoxical-autumn@users.noreply.github.com>
2025-01-04 13:12:02 +0000

    generic component from slot

---
+++ ArbitraryComponentAccess/Bindings/ProtoFlux/Components/ComponentFromSlot.cs
@@ -0,0 +1,71 @@
+﻿using FrooxEngine;
+using ProtoFlux.Core;
+using FrooxEngine.ProtoFlux;
+using FrooxEngine.ProtoFlux.Runtimes.Execution;
+using ArbitraryComponentAccess.ProtoFlux.Components;
+using Elements.Core;
+
+using FluxExecutionContext = ProtoFlux.Runtimes.Execution.ExecutionContext;
+
+namespace ArbitraryComponentAccess.ProtoFluxBinds.Components;
+
+[Category("ProtoFlux/Runtimes/Execution/Nodes/ACA/Components")]
+public class ComponentFromSlot : ObjectFunctionNode<FrooxEngineContext, Component>
+{
+    public readonly SyncRef<INodeObjectOutput<Slot>> slot = new();
+    public readonly SyncRef<INodeObjectOutput<Type>> componentType = new();
+    public readonly SyncRef<INodeValueOutput<int>> componentIndex = new();
+
+    public ComponentFromSlotLogix TypedNodeInstance { get; private set; } = null!;
+    public override Type NodeType => typeof(ComponentFromSlotLogix);
+    public override INode NodeInstance => TypedNodeInstance;
+    public override int NodeInputCount => base.NodeInputCount + 3;
+
+    public override void ClearInstance()
+    {
+        TypedNodeInstance = null!;
+    }
+
+    public override N Instantiate<N>()
+    {
+        if (TypedNodeInstance != null)
+        {
+            throw new InvalidOperationException("Node has already been instantiated");
+        }
+
+        TypedNodeInstance = new ComponentFromSlotLogix();
+        return (TypedNodeInstance as N)!;
+    }
+
+    protected override void AssociateInstanceInternal(INode node)
+    {
+        if (node is ComponentFromSlotLogix typedNodeInstance)
+        {
+            TypedNodeInstance = typedNodeInstance;
+            return;
+        }
+        throw new ArgumentException("Node instance is not of type " + typeof(ComponentFromSlotLogix));
+    }
+
+    protected override ISyncRef GetInputInternal(ref int index)
+    {
+        ISyncRef inputInternal = base.GetInputInternal(ref index);
+        if (inputInternal != null)
+        {
+            return inputInternal;
+        }
+
+        switch (index)
+        {
+            case 0:
+                return slot;
+            case 1:
+                return componentType;
+            case 2:
+                return componentIndex;
+            default:
+                index -= 3;
+                return null!;
+        }
+    }
+}
---
+++ ArbitraryComponentAccess/ProtoFlux/Components/ComponentFromSlot.cs
@@ -0,0 +1,47 @@
+﻿// no auto
+using ArbitraryComponentAccess.Components;
+using Elements.Core;
+using FrooxEngine;
+using FrooxEngine.ProtoFlux;
+using ProtoFlux.Core;
+using ProtoFlux.Runtimes.Execution;
+
+namespace ArbitraryComponentAccess.ProtoFlux.Components;
+
+[NodeCategory("ACA/Components")]
+public class ComponentFromSlotLogix : ObjectFunctionNode<FrooxEngineContext, Component>
+{
+    public ObjectInput<Slot> slot;
+    public ObjectInput<Type> componentType;
+    public ValueInput<int> componentIndex;
+
+    protected override Component Compute(FrooxEngineContext context)
+    {
+        if (!context.World.Permissions.CheckAll((ACAPermissions p) => p.is_ACA_Allowed))
+        {
+            return null!;
+        }
+
+        Slot? my_slot = slot!.Evaluate(context);
+        Type? my_type = componentType!.Evaluate(context);
+        int my_index = componentIndex!.Evaluate(context, 0);
+
+        // handle nulls
+        if (my_slot == null) return null!;
+        if (my_type == null) return null!;
+
+        if (!my_type.IsSubclassOf(typeof(Component)) || my_type.GetConstructor(Type.EmptyTypes) == null || my_type.ContainsGenericParameters)
+            return null!;
+
+        if (!context.World.Permissions.CheckAll((ACAPermissions p) => p.IsTypeAllowedForExecution(my_type)))
+        {
+            return null!;
+        }
+
+        List<Component> my_comps = my_slot.GetComponents(my_type.ToString()).ToList();
+
+        if (my_index < 0 || my_index >= my_comps.Count) return null!;
+
+        return my_comps.GetOrNull(my_index);
+    }
+}
