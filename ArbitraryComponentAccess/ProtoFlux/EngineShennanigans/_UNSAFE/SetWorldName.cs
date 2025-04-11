cc53f5e3a76634f5b2adfab4631c036fe9c839d9
paradoxical-autumn <110392755+paradoxical-autumn@users.noreply.github.com>
2025-04-11 14:43:28 +0100

    set world name from pfx - req. from anonymous user
    THIS IS CURRENTLY INSECURE AND UNTESTED. DO NOT SHIP.

---
+++ ArbitraryComponentAccess/ProtoFlux/EngineShennanigans/_UNSAFE/SetWorldName.cs
@@ -0,0 +1,41 @@
+﻿using FrooxEngine;
+using FrooxEngine.ProtoFlux;
+using ProtoFlux.Core;
+using ProtoFlux.Runtimes.Execution;
+
+namespace ArbitraryComponentAccess.ProtoFlux;
+
+[NodeCategory("EngineShennanigans")]
+public class SetWorldNameLogix : ActionNode<FrooxEngineContext>
+{
+    public ObjectInput<string> newName;
+
+    public Continuation onSuccess;
+    public Continuation onFailure;
+
+    protected override IOperation Run(FrooxEngineContext context)
+    {
+        string worldName = newName!.Evaluate(context)!;
+
+        if (worldName == null)
+        {
+            return onFailure.Target;
+        }
+
+        if (!context.World.IsAuthority)
+        {
+            return onFailure.Target;
+        }
+
+        try
+        {
+            context.World.Configuration.WorldName.Value = worldName;
+            return onSuccess.Target;
+        }
+        catch (Exception ex)
+        {
+            UniLogPlugin.Error($"error in setting world thing: {ex}");
+            return onFailure.Target;
+        }
+    }
+}
