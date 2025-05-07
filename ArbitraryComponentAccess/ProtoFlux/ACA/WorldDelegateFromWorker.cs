f49642f57fce05a42759e9de19d0a2c4db53e6ce
ErrorJan <50844353+ErrorJan@users.noreply.github.com>
2025-05-07 13:27:48 +0200

    Added a way to get a delegate from a IWorldElement.

---
+++ ArbitraryComponentAccess/ProtoFlux/ACA/WorldDelegateFromWorker.cs
@@ -0,0 +1,20 @@
+﻿using FrooxEngine;
+using FrooxEngine.ProtoFlux;
+using ProtoFlux.Core;
+using ProtoFlux.Runtimes.Execution;
+
+namespace ArbitraryComponentAccess.ProtoFlux.Fields;
+
+[NodeCategory("ACA")]
+public class BoxedWorldDelegateFromIWorldElementLogix : ObjectFunctionNode<FrooxEngineContext, object>
+{
+    public ObjectInput<IWorldElement> iWorldElement;
+    public ObjectInput<string> delegateName;
+
+    protected override object Compute(FrooxEngineContext context)
+    {
+        IWorldElement? cObj = iWorldElement!.Evaluate(context);
+        string? cName = delegateName!.Evaluate(context);
+        return new WorldDelegate( cObj?.ReferenceID ?? 0, cName ?? "", null! );
+    }
+}
